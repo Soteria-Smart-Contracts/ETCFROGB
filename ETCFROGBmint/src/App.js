60853e2356d5609fd314e49a97765ff791e7de1f
Soteria Smart Contracts <62032531+Soteria-Smart-Contracts@users.noreply.github.com>
2022-06-25 22:02:12 -0400

    Update App.js

--- ETCFROGBmint/src/App.js
+++ ETCFROGBmint/src/App.js
@@ -120,37 +120,6 @@ function App() {
     SHOW_BACKGROUND: false,
   });
 
-  const claimNFTs = () => {
-    let cost = CONFIG.WEI_COST;
-    let gasLimit = CONFIG.GAS_LIMIT;
-    let totalCostWei = String(cost * mintAmount);
-    let totalGasLimit = String(gasLimit * mintAmount);
-    console.log("Cost: ", totalCostWei);
-    console.log("Gas limit: ", totalGasLimit);
-    setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
-    setClaimingNft(true);
-    blockchain.smartContract.methods
-      .mint(blockchain.account, mintAmount)
-      .send({
-        gasLimit: String(totalGasLimit),
-        to: CONFIG.CONTRACT_ADDRESS,
-        from: blockchain.account,
-        value: totalCostWei,
-      })
-      .once("error", (err) => {
-          console.log(err);
-          setFeedback("Sorry, something went wrong please try again later.");
-          setClaimingNft(false);
-        })
-        .then((receipt) => {
-          console.log(receipt);
-          setFeedback(
-            `WOW, the ${CONFIG.NFT_NAME} is yours! go visit Opensea.io to view it.`
-          );
-          setClaimingNft(false);
-          dispatch(fetchData(blockchain.account));
-        });
-  };
 
   const decrementMintAmount = () => {
     let newMintAmount = mintAmount - 1;
