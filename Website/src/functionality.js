b450abd02136df3a11856314d171c5b94ac4e518
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-28 11:59:14 -0400

    June 28, 2022 11:59 AM

--- Website/src/functionality.js
+++ Website/src/functionality.js
@@ -6,8 +6,8 @@ async function loginWithEth(){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = await new Web3(ethereum);
         await getID();
-        if (netID != 66){
-            console.log("The current Metamask/Web3 network is not Ropsten, please connect to the Ropsten test network."); //CHANGE FOR REAL CROWDSALE TO ETC
+        if (netID != 61){
+            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ropsten test network."); //CHANGE FOR REAL CROWDSALE TO ETC
             alert("The current Metamask/Web3 network is not Ropsten, please connect to the Ropsten test network.");
             showOverlay();
             return("Failed to connect")
