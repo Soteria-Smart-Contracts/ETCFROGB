c2b9a7adbb26de471bbf3ae8407bf4acc4e401d2
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-29 10:05:09 -0400

    June 29, 2022 10:05 AM

--- Website/src/functionality.js
+++ Website/src/functionality.js
@@ -19,6 +19,7 @@ async function loginWithEth(){
         accountarray = await web3.eth.getAccounts();
         contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
+        getsupply();
     } else {
         alert("No ETHER Wallet available")
     }
