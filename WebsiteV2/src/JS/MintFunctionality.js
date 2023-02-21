3871252f26e68f7457a73861435892b3ee0e4518
Darius Tanz <scratchticketseb@gmail.com>
2023-02-21 19:06:14 +0000

    Feb 21, 2023, 2:06 PM

--- WebsiteV2/src/JS/MintFunctionality.js
+++ WebsiteV2/src/JS/MintFunctionality.js
@@ -44,7 +44,8 @@ async function getID(){
 async function mint(){
     let amount = document.getElementById("amount").value;
     let amountWei = amount * 2000000000000000000;
-    if(amountWei > )
+    let userbal = await web3.eth.getBalance(account);
+    if(amountWei > await web3.eth.getBalance(account))
     let gas = await contract.methods.mint(amount).estimateGas({from: account});
     let tx = await contract.methods.mint(amount).send({from: account, value: amountWei, gas: gas});
 }
