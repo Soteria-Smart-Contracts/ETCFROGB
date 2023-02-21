00df7d50064ab4f8ecd9c172b8d039451322dd46
Darius Tanz <scratchticketseb@gmail.com>
2023-02-21 19:15:13 +0000

    Feb 21, 2023, 2:15 PM

--- WebsiteV2/src/JS/MintAnimations.js
+++ WebsiteV2/src/JS/MintAnimations.js
@@ -6,6 +6,6 @@ WalletButton = document.getElementById('Wallet')
 
 async function DisplayLoggedIn(){
     MintButton.innerText = "Mint";
-    MintButton.onclick = function mint();
+    MintButton.onclick = mint();
     WalletButton.innerText = "Connected"
 }
