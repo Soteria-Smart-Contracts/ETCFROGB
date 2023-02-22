82fb3f3bfdf0f25e501137f002d8e550b61786b9
Darius Tanz <scratchticketseb@gmail.com>
2023-02-22 13:53:45 +0000

    Feb 22, 2023, 8:53 AM

--- WebsiteV2/src/JS/MyFrogsViewer.js
+++ WebsiteV2/src/JS/MyFrogsViewer.js
@@ -3,7 +3,7 @@ const ABI = window.abi;
 let netID;
 const contractAddress = "0x5925630e4D0AB569A40E600064Da2930b4838Da3";
 let LoggedIn = false;
-let 
+let NFTs = [];
 
 let WalletButton = document.getElementById('Wallet');
 
@@ -40,4 +40,4 @@ async function getID(){
     return(netID);
 }
 
-async function GetUserNFTs
+async function GetUserNFTs()
