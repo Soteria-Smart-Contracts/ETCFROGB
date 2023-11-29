bbec252e079939a8d3dd88f456f55d9376d2e97d
Darius Tanz <scratchticketseb@gmail.com>
2023-11-29 21:22:29 +0000

    Nov 29, 2023, 4:22 PM

--- WebsiteV2/src/JS/MyFrogsViewer.js
+++ WebsiteV2/src/JS/MyFrogsViewer.js
@@ -58,7 +58,7 @@ async function DisplayNFT(ID){
     NewNFT.id = ID;
     let img = '<img class="nftimage" src="https://etcfrogb.mypinata.cloud/ipfs/QmdHWsMWde4UCTiQJojN2ed3rBmxFAsjQkKZDi4dCZZB3F/' + ID + '.png">'
     let ScoutLink = 'https://blockscout.com/etc/mainnet/token/0x5925630e4D0AB569A40E600064Da2930b4838Da3/instance/' + ID +  '/token-transfers';
-    NewNFT.innerHTML = img + '<br>' + '<a class="nftID">FrogB ID:</a>' + '<br>' + '<a class="nftID">' + ID + '</a>' + '<br>' + '<a class="nftID scout" href="' + ScoutLink + '" target="_blank">View On Blockscout</a>'  //add a button thats says claim;
+    NewNFT.innerHTML = img + '<br>' + '<a class="nftID">FrogB ID:</a>' + '<br>' + '<a class="nftID">' + ID + '</a>' + '<br>' + '<a class="nftID scout" href="' + ScoutLink + '" target="_blank">View On Blockscout</a>' +   //add a button thats says claim;
     NFTContainer.appendChild(NewNFT);
 }
 
