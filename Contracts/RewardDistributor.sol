89b56c925fb32cc7daeda1374fa725ff320e3265
Darius Tanz <scratchticketseb@gmail.com>
2023-12-05 15:41:01 +0000

    Dec 5, 2023, 10:41 AM

--- Contracts/RewardDistributor.sol
+++ Contracts/RewardDistributor.sol
@@ -16,7 +16,7 @@ contract NFTRewardDistributor is ReentrancyGuard{
     mapping(uint256 => uint256) public LatestClaim;
     mapping(address => uint256) public UserTotalClaimed;
     mapping(uint256 => bool) internal FirstClaimComplete;
-    mapping(address => uint256[]) public 
+    mapping(address => uint256[]) public UserRegisteredTokens;
 
     event ClaimedAllRewards(uint256 TotalReward, address User);
     event NewInstanceCreated(RewardInstance NewInstanceDetails);
