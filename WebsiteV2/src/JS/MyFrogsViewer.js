8329a6b3899f5276081eb248dc63bc8e76c5a902
Darius Tanz <scratchticketseb@gmail.com>
2023-12-09 20:15:03 +0000

    Dec 9, 2023, 3:15 PM

--- WebsiteV2/src/JS/MyFrogsViewer.js
+++ WebsiteV2/src/JS/MyFrogsViewer.js
@@ -112,7 +112,7 @@ async function DisplayNFT(ID){
     else{
         isRegistered = "Not Registered";
     }
-    NewNFT.innerHTML = img + '<br>' + '<a class="nftID">FrogB ID:</a>' + '<br>' + '<a class="nftID">' + ID + '</a>' + '<br>' + '<a class="nftID scout" href="' + ScoutLink + '" target="_blank">View On Blockscout</a>' + //add a button thats says claim;
+    NewNFT.innerHTML = img + '<br>' + '<a class="nftID">FrogB ID:</a>' + '<br>' + '<a class="nftID">' + ID + '</a>' + '<br>' + '<a class="nftID scout" href="' + ScoutLink + '" target="_blank">View On Blockscout</a>' + '<br>' + Is//add a button thats says claim;
     NFTContainer.appendChild(NewNFT);
 }
 
