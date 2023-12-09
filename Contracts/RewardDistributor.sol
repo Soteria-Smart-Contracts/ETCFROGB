59895a0feef79cd330ee45c7c4b29a99e530f4b8
Darius Tanz <scratchticketseb@gmail.com>
2023-12-09 20:39:48 +0000

    Dec 9, 2023, 3:39 PM

--- Contracts/RewardDistributor.sol
+++ Contracts/RewardDistributor.sol
@@ -245,7 +245,7 @@ contract NFTRewardDistributor is ReentrancyGuard{
                 if (UserRegisteredTokens[msg.sender].length > 1) {
                     UserRegisteredTokens[msg.sender][UserRegisteredTokensIndex[msg.sender][Tokens[index]]] = UserRegisteredTokens[msg.sender][UserRegisteredTokens[msg.sender].length - 1];
                     UserRegisteredTokens[msg.sender].pop();
-                } else if (UserRegisteredTokens[msg.sender].length == 1) {
+                else{
                     UserRegisteredTokens[msg.sender].pop();
                 }
                 TokenRegistered[Tokens[index]] = false;
